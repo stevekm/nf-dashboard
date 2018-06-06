c72fefcc5a163e6d18da5f9abf21822ce4f5fee2
Stephen K <steve.at.nyu1@gmail.com>
2018-06-05 22:28:38 -0400

    add start workflow to client and server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var axios = require('axios')
+const { spawn } = require('child_process');
 const serverPort = process.argv[2]; // 8080
 const apiUrl = process.argv[3]; // http://localhost
 const apiPort = process.argv[4]; // 5000
@@ -43,6 +44,26 @@ app.get('/message/:id', (req, res) => {
 	    message: JSON.stringify(message, null, 4)
 	});
   });
+});
+
+// start a new workflow
+app.post('/start', function(req, res){
+	// console.log(req.body);
+	console.log('>>> Starting new workflow...')
+	const child = spawn('make', ['launch-nextflow', `APIPORT=${apiPort}`])
+
+	child.stdout.on('data', (data) => {
+	  console.log(`${data}`);
+	});
+
+	child.stderr.on('data', (data) => {
+	  console.error(`${data}`);
+	});
+
+	child.on('exit', function (code, signal) {
+	console.log('>>> Child workflow process exited with ' + `code ${code} and signal ${signal}`);
+	  res.sendStatus(200);
+	});
 });
 
 app.listen(serverPort);
