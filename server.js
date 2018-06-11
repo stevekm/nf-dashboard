9d89907c6217a8e85eb28781cb40461adcadb19d
Stephen K <steve.at.nyu1@gmail.com>
2018-06-11 16:35:41 -0400

    add websockets update whitespace

--- server.js
+++ server.js
@@ -3,37 +3,37 @@ var app = express();
 var bodyParser = require('body-parser');
 var axios = require('axios')
 const { spawn } = require('child_process');
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
 const serverPort = process.argv[2]; // 8080
 const apiUrl = process.argv[3]; // http://localhost
 const apiPort = process.argv[4]; // 5000
 
+
+// ~~~~~ GLOBALS ~~~~~ //
 var numRunningWorkflows = 0;
 
-// ~~~~~ web socket ~~~~~ //
-// var WebSocketServer = require('ws').Server,
-//   wss = new WebSocketServer({port: 40510})
-// wss.on('connection', function (ws) {
-//   ws.on('message', function (message) {
-//     console.log('web socket received: %s', message)
-//   })
-//
-//   // setInterval(
-//   //   () => ws.send(`${new Date()}`),
-//   //   1000
-//   // )
-// })
+// ~~~~~ web socket server ~~~~~ //
+var WebSocketServer = require('ws').Server
+const wss = new WebSocketServer({port: 40510})
 
+wss.on('connection', function (ws) {
+    ws.on('message', function (message) {
+        console.log('web socket received: %s', message);
+    })
 
-// const WebSocket = require('ws');
-// const ws = new WebSocket('ws://www.host.com/path', {
-//   perMessageDeflate: false
-// });
-const WebSocket = require('ws');
-const ws = new WebSocket('ws://localhost:40510');
+    eventEmitter.on("start-workflow", function(){
+        numRunningWorkflows = numRunningWorkflows + 1;
+        ws.send(`${numRunningWorkflows}`);
+    });
 
-ws.on('open', function open() {
-  ws.send('something');
+    eventEmitter.on("end-workflow", function(){
+        numRunningWorkflows = numRunningWorkflows - 1;
+        ws.send(`${numRunningWorkflows}`);
+    });
 });
+
 
 // ~~~~~~~ WEB SERVER ~~~~~~ //
 // viewed at http://localhost:8080
@@ -41,62 +41,59 @@ app.set('view engine', 'ejs');
 
 // app home page
 app.get('/', function(req, res) {
-	// get a list of workflows from the API
-	var url = apiUrl + ':' + apiPort + '/workflows/';
-	axios.get(url)
-	  .then(function(response){
-		res.render('index', { runs: response.data})
-	});
+    // get a list of workflows from the API
+    var url = apiUrl + ':' + apiPort + '/workflows/';
+    axios.get(url)
+        .then(function(response){
+            res.render('index', { runs: response.data})
+    });
 });
 
 // workflow page
 app.get('/workflow/:runid', function(req, res) {
-	// get the messages for a specific workflow
-	var runid = req.params.runid;
-	var url = apiUrl + ':' + apiPort + '/workflow/' + runid;
-	axios.get(url)
-	  .then(function(response){
-			res.render('workflow', { messages: response.data, runid: runid})
-		});
+    // get the messages for a specific workflow
+    var runid = req.params.runid;
+    var url = apiUrl + ':' + apiPort + '/workflow/' + runid;
+    axios.get(url)
+      .then(function(response){
+            res.render('workflow', { messages: response.data, runid: runid})
+        });
 });
 
 // single message
 app.get('/message/:id', (req, res) => {
-  // get a message from the API to display on the page
-  var messageid = req.params.id;
-  var url = apiUrl + ':' + apiPort + '/message/' + messageid;
-  axios.get(url)
-	.then(function(response){
-		var message = response.data;
-	  res.render('message', {
-	    message: JSON.stringify(message, null, 4)
-	});
-  });
+    // get a message from the API to display on the page
+    var messageid = req.params.id;
+    var url = apiUrl + ':' + apiPort + '/message/' + messageid;
+    axios.get(url)
+    .then(function(response){
+        var message = response.data;
+        res.render('message', {
+            message: JSON.stringify(message, null, 4)
+        });
+    });
 });
 
 // start a new workflow
 app.post('/start', function(req, res){
-	// console.log(req.body);
-	console.log('>>> Starting new workflow...')
-	const child = spawn('make', ['launch-nextflow', `APIPORT=${apiPort}`])
-	numRunningWorkflows = numRunningWorkflows + 1;
-	ws.send(`${numRunningWorkflows}`)
-	console.log(`${numRunningWorkflows} running workflows`);
+    // console.log(req.body);
+    console.log('>>> Starting new workflow...');
+    const child = spawn('make', ['launch-nextflow', `APIPORT=${apiPort}`]);
+    eventEmitter.emit("start-workflow");
 
-	child.stdout.on('data', (data) => {
-	  console.log(`${data}`);
-	});
+    child.stdout.on('data', (data) => {
+        console.log(`${data}`);
+    });
 
-	child.stderr.on('data', (data) => {
-	  console.error(`${data}`);
-	});
+    child.stderr.on('data', (data) => {
+        console.error(`${data}`);
+    });
 
-	child.on('exit', function (code, signal) {
-	console.log('>>> Child workflow process exited with ' + `code ${code} and signal ${signal}`);
-	numRunningWorkflows = numRunningWorkflows - 1;
-	  res.sendStatus(200);
-	  ws.send(`${numRunningWorkflows}`)
-	});
+    child.on('exit', function (code, signal) {
+    console.log('>>> Child workflow process exited with ' + `code ${code} and signal ${signal}`);
+        res.sendStatus(200);
+        eventEmitter.emit("end-workflow");
+    });
 });
 
 app.listen(serverPort);
